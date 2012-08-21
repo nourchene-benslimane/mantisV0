9517243780d00e3a14a0090e948766089bb4f5bd
nourchene <nourchene.benslimane@gmail.com>
2012-08-21 09:08:45 +0100

    mantis version basique

---
+++ mantis/config_inc.php
@@ -0,0 +1,16 @@
+<?php
+	$g_hostname = 'localhost';
+	$g_db_type = 'mysql';
+	$g_database_name = 'bugtracker';
+	$g_db_username = 'root';
+	$g_db_password = 'root';
+	
+	$g_default_language='french'; 
+	$g_short_date_format='d-m-Y'; 
+	$g_normal_date_format='d-m-Y H:i'; 
+	$g_complete_date_format='d-m-Y H:i'; 
+	
+	$g_logo_image			= 'images/btl-baniere.jpg';
+
+	
+?>
